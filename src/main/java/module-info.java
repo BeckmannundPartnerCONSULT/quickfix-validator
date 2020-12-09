6645306a459e9f070b39eb8d0654d654035eafd0
Jens <jkh@beckmann-partner.de>
2020-12-09 19:37:38 +0100

    added missing modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module de.beckdev.quickfix {
   requires java.xml;
   requires java.xml.bind;
+  requires orchestra.repository;
+  requires orchestra.score;
+  requires quickfixj.core;
+  requires message.model;
 
   exports de.beckdev.quickfix.validation;
 }
